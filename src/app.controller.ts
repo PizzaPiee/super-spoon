f0684b8bf1c8f4cc030e94bbf9a5fc8f708cea2d
Maaz Ullah <ullahmaaz3@gmail.com>
2022-08-09 14:34:21 +0200

    add user decorator to /profile route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { JwtPayload } from './auth/types';
+import { User } from './common/decorators';
 
 @Controller()
 export class AppController {
     @UseGuards(JwtAuthGuard)
     @Get('profile')
-    getProfile(@Request() req) {
-        return req.user;
+    getProfile(@User() user: JwtPayload) {
+        return user;
     }
 }
